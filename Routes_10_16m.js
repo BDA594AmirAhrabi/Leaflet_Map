c0d41f0297bd619b87f984cfbaa37cc5f5c4fe24
BDA594AmirAhrabi <aahrabi8122@sdsu.edu>
2025-06-14 11:57:28 -0700

    Add files via upload

--- Routes_10_16m.js
+++ Routes_10_16m.js
@@ -1,15 +1,4 @@
-var routes_10_16m = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "LineString",
-        "coordinates": [
-          [-76.620, 39.31],
-          [-76.630, 39.33]
-        ]
-      }
-    }
-  ]
-};                       
+var routes_10_16m =
+
+// put your lines in json format here
+
